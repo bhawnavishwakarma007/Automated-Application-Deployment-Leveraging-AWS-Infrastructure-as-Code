30ff50858dde3935aa6dc70575f992b8956da4ff
bhawna <bhawnavishwakarma464@gmail.com>
2026-03-10 00:28:20 +0530

    day-1-terraform

---
+++ day-1/imports.tf
@@ -0,0 +1,4 @@
+import {
+  to = aws_s3_bucket.tf-bucket
+  id = "how-to-train-ur-dragon"
+}
---
+++ day-1/provider.tf
@@ -0,0 +1,18 @@
+provider "aws" {
+  region = "us-east-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::875778602097:role/tf-role"
+    session_name = "tf-access"
+  }
+}
+
+terraform {
+  required_version = "1.14.3"
+  required_providers {
+    aws = {
+      version = "~>6.0.0"
+      source = "hashicorp/aws"
+    }
+  }
+}
